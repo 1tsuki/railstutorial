a001938b453195b9283cb8225fffee1404594403
Itsuki Sakitsu <sakitsu@r.recruit.co.jp>
2018-09-08 22:19:18 +0900

    finished chapter 8

--- environment/sample_app/test/integration/users_signup_test.rb
+++ environment/sample_app/test/integration/users_signup_test.rb
@@ -6,10 +6,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select 'form[action="/signup"]'
     assert_no_difference 'User.count' do
-      post signup_path, params: { user: {name: "",
-                          email: "user@invalid",
-                          password: "foo",
-                          password_confirmation: "bar"}}
+      post signup_path, params: {user: {name: "",
+                                        email: "user@invalid",
+                                        password: "foo",
+                                        password_confirmation: "bar"}}
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
@@ -20,14 +20,15 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name:  "Example User",
-                                         email: "user@example.com",
-                                         password:              "password",
-                                         password_confirmation: "password" } }
+      post users_path, params: {user: {name: "Example User",
+                                       email: "user@example.com",
+                                       password: "password",
+                                       password_confirmation: "password"}}
     end
     follow_redirect!
     assert_template 'users/show'
     assert_not flash[:success].blank?
+    assert is_logged_in?
   end
 
 end
