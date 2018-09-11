cfc8ac8e4fd17c9a59fc781772bf29ae7e11285f
Itsuki Sakitsu <sakitsu@r.recruit.co.jp>
2018-09-11 21:28:07 +0900

    finished chapter 11

--- environment/sample_app/test/integration/users_signup_test.rb
+++ environment/sample_app/test/integration/users_signup_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_select 'form[action="/signup"]'
@@ -17,7 +21,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors'
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: {user: {name: "Example User",
@@ -25,10 +29,25 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                        password: "password",
                                        password_confirmation: "password"}}
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not is_logged_in?
+
+    # 有効化していない状態でのログイン試行
+    log_in_as(user)
+    assert_not is_logged_in?
+    # 有効化トークンが不正な場合
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    # トークンは正しいがメールアドレスが無効な場合
+    get edit_account_activation_path(user.activation_token, email: "wrong")
+    assert_not is_logged_in?
+    # 有効化トークンが正しい場合
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
     assert_template 'users/show'
     assert_not flash[:success].blank?
     assert is_logged_in?
   end
-
 end
